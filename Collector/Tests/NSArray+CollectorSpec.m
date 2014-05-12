f47baacc8417050272cf4f8a8e341c88ef37198f
Michaël Fortin <fortinmike@irradiated.net>
2014-05-11 23:38:01 -0400

    Added Kiwi test for -first:.

--- Collector/Tests/NSArray+CollectorSpec.m
+++ Collector/Tests/NSArray+CollectorSpec.m
@@ -18,12 +18,25 @@ describe(@"NSArray+Collector", ^
 	{
 		it(@"should return the first object when there are objects in the receiver", ^
 		{
-			[[[@[@1, @2, @3] firstObject] should] equal:@1];
+			[[[@[@1, @2, @3] first] should] equal:@1];
 		});
 		
 		it(@"should return nil when there are no objects in the array", ^
 		{
-			[[[@[] firstObject] should] beNil];
+			[[[@[] first] should] beNil];
+		});
+	});
+	
+	context(@"first with condition", ^
+	{
+		it(@"should return the first matching object if there are objects matching the condition in the array", ^
+		{
+			[[[@[@1, @"Foo", @2, @"Bar"] first:^BOOL(id object) { return [object isKindOfClass:[NSString class]]; }] should] equal:@"Foo"];
+		});
+		
+		it(@"should return nil if there are no objects matching the condition in the array", ^
+		{
+			[[[@[@1, @"Foo"] first:^BOOL(id object) { return [object isKindOfClass:[NSData class]]; }] should] beNil];
 		});
 	});
 });
