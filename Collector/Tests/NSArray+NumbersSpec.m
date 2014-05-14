b37a8902435c5d20b6671c54227d6426d2959f7e
Michaël Fortin <fortinmike@irradiated.net>
2014-05-13 23:18:55 -0400

    Implemented NSArray+NumbersSpec.

--- Collector/Tests/NSArray+NumbersSpec.m
+++ Collector/Tests/NSArray+NumbersSpec.m
@@ -12,23 +12,37 @@
 
 SPEC_BEGIN(NSArray_NumbersSpec)
 
-//- (void)testMin
-//{
-//	NSArray *numbers = @[@1, @5, @10, @3, @(-1), @22, @1, @55, @3.2];
-//	NSNumber *min = [numbers min];
-//	XCTAssertEqualObjects(min, @(-1), @"Min value is wrong");
-//}
-//
-//- (void)testMax
-//{
-//	NSArray *numbers = @[@1, @5, @10, @3, @(-1), @22, @1, @55, @3.2];
-//	NSNumber *max = [numbers max];
-//	XCTAssertEqualObjects(max, @55, @"Max value is wrong");
-//}
-
 describe(@"NSArray+Numbers", ^
 {
+	context(@"min", ^
+	{
+		it(@"should return the instance of NSNumber with the lowest value in the array", ^
+		{
+			NSArray *numbers = @[@1, @5, @10, @3, @(-1), @22, @1, @55, @3.2];
+			
+			[[[numbers min] should] equal:@(-1)];
+		});
+	});
 	
+	context(@"max", ^
+	{
+		it(@"should return the instance of NSNumber with the highest value in the array", ^
+		{
+			NSArray *numbers = @[@1, @5, @10, @3, @(-1), @22, @1, @55, @3.2];
+			
+			[[[numbers max] should] equal:@(55)];
+		});
+	});
+	
+	context(@"sum", ^
+	{
+		it(@"should return the sum of all NSNumbers the array", ^
+		{
+			NSArray *numbers = @[@1, @5, @10, @3, @(-1), @22, @1, @55, @3.2];
+			
+			[[[numbers sum] should] equal:99.2 withDelta:FLT_EPSILON];
+		});
+	});
 });
 
 SPEC_END
