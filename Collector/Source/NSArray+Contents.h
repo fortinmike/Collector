536cece8ebe2341590f7a176aabe3e7bb2ee677d
Michaël Fortin <fortinmike@irradiated.net>
2014-05-14 19:29:39 -0400

    Documentation for NSArray+Contents.

--- Collector/Source/NSArray+Contents.h
+++ Collector/Source/NSArray+Contents.h
@@ -12,9 +12,38 @@
 
 #pragma mark Array Contents Checking
 
-- (BOOL)containsObjects:(NSArray *)array; // Returns whether the array contains the same objects as the parameter or more
-- (BOOL)containsOnlyObjects:(NSArray *)array; // Returns whether the array contains exactly the same objects as the parameter, not more
+/**
+ *  Checks whether the receiver contains all of the given objects.
+ *
+ *  @param array The objects to search for.
+ *
+ *  @return YES if all of the objects are found in the receiver.
+ */
+- (BOOL)containsObjects:(NSArray *)array;
+
+/**
+ *  Checks if the receiver contains *only* objects in the given array, regardless of their order.
+ *
+ *  @param array The objects to search for.
+ *
+ *  @return YES if the receiver contains all of the objects, but no more.
+ */
+- (BOOL)containsOnlyObjects:(NSArray *)array;
+
+/**
+ *  Checks if the receiver contains one or more of the given objects.
+ *
+ *  @param objects The objects to search for.
+ *
+ *  @return YES if the receiver contains at least one of the receiver's objects.
+ */
 - (BOOL)containsAnyObject:(NSArray *)objects;
+
+/**
+ *  Checks whether all objects in the array are of the specified kind.
+ *
+ *  @return YES if all objects are of kind *klass*.
+ */
 - (BOOL)areObjectsKindOfClass:(Class)klass; // Checks whether all objects in the array are of the specified class or any of its subclasses
 
 @end
