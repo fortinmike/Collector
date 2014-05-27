3604fe9676023229e1ce46116d074d106cdca352
Michaël Fortin <fortinmike@irradiated.net>
2014-05-27 00:26:06 -0400

    Modified path to workspace in travis-build.sh.

--- Scripts/travis-build.sh
+++ Scripts/travis-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace ../Collector/Collector.xcworkspace -scheme Collector.iOS test
-xctool -workspace ../Collector/Collector.xcworkspace -scheme Collector.Mac test
+xctool -workspace Collector/Collector.xcworkspace -scheme Collector.iOS test
+xctool -workspace Collector/Collector.xcworkspace -scheme Collector.Mac test
