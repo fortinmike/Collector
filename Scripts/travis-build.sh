2a89369e6521495e8848c327ecddae92473be633
Michaël Fortin <fortinmike@irradiated.net>
2014-05-27 00:24:19 -0400

    Set proper workspace filename in travis-build.sh.

--- Scripts/travis-build.sh
+++ Scripts/travis-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace ../Collector/MyApp.xcworkspace -scheme Collector.iOS test
-xctool -workspace ../Collector/MyApp.xcworkspace -scheme Collector.Mac test
+xctool -workspace ../Collector/Collector.xcworkspace -scheme Collector.iOS test
+xctool -workspace ../Collector/Collector.xcworkspace -scheme Collector.Mac test
