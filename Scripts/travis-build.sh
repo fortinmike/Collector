0a6e866841eceb8bf2a36bb71d2be949e3ffa1d8
Michaël Fortin <fortinmike@irradiated.net>
2014-05-27 00:28:59 -0400

    Added "pod install" to travis-build.sh.

--- Scripts/travis-build.sh
+++ Scripts/travis-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
+pod install
 xctool -workspace Collector/Collector.xcworkspace -scheme Collector.iOS test
 xctool -workspace Collector/Collector.xcworkspace -scheme Collector.Mac test
